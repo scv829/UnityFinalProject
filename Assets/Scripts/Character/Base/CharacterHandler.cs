baa74e06110070b1911dbc5cde67b2e92e1c541e
scv829 <scv829@naver.com>
2025-02-26 12:02:19 +0900

    Feat: 캐릭터 기본적인 스펙 설계
    캐릭터 기본적인 스펙 설계
    
    Related to: #1

---
+++ Assets/Scripts/Character/Base/CharacterHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 공통으로 사용할 캐릭터 핸들러
+/// </summary>
+public class CharacterHandler : MonoBehaviour, IHit
+{
+    [SerializeField] CharacterSO characterData;
+
+    [SerializeField] private float currentHp;
+
+    private void Start()
+    {
+        currentHp = characterData.HP;
+    }
+
+    public void TakeDamage(GameObject requester, BattleEnum.DamageType type, float damage)
+    {
+        currentHp -= damage;
+    }
+
+    /// <summary>
+    /// 사망 여부 확인
+    /// </summary>
+    public bool IsDied => currentHp <= 0;
+
+}
---
+++ Assets/Scripts/Character/Base/CharacterSO.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CharacterEnum;
+
+[CreateAssetMenu(menuName = "Character/Data")]
+public class CharacterSO : ScriptableObject
+{
+    [SerializeField] int character_id;
+    [SerializeField] string character_name;
+    [SerializeField] int character_star;
+    [SerializeField] string character_description;
+    [SerializeField] AttackType character_type;
+    [SerializeField] float hp;
+    [SerializeField] List<Skill> character_skill;
+    
+    #region 프로퍼티
+    public string Name => character_name;
+    public int CharacterId => character_id;
+    public string CharacterName => character_name;
+    public int CharacterStar => character_star;
+    public string CharacterDescription => character_description;
+    public AttackType CharacterType => character_type;
+    public float HP => hp;
+    public List<Skill> CharacterSkill => character_skill;
+    #endregion
+
+}
