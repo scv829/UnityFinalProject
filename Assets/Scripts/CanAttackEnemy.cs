94ee145cf74ced42112b4b5d6e62b437f6b3aea2
scv829 <scv829@naver.com>
2025-02-28 12:12:21 +0900

    Refactor: 이동 및 공격가능 로직 수정
    - 이동 및 공격가능 로직 수정
    - 하나의 노드로 두 방향에 대한 연산이 가능하게 수정
    - 방향은 SpriteRenderer의 flip을 기준으로 설정
    
    Related to: #1

--- Assets/Scripts/CanAttackEnemy.cs
+++ Assets/Scripts/CanAttackEnemy.cs
@@ -1,76 +1,90 @@
-using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
 using TooltipAttribute = BehaviorDesigner.Runtime.Tasks.TooltipAttribute;
 
 [TaskCategory("Battle")]
 public class CanAttackEnemy : Conditional
 {
-	// 하는 일
-	// 적이 공격 범위 안에 있는지 확인
-	// 있으면 해당 적을 타겟을 설정 및 성공 반환
-	// 없으면 null로 하고 실패 반환
+    [Tooltip("공격 범위에 들어온 적 캐릭터")]
+    public SharedGameObject Target;
+    [Tooltip("공격 범위에 들어온 적 캐릭터의 태그")]
+    public SharedString TargetTag;
 
-	// 할 수 있는 방법 2가지
-	// 1. Physic2D.overlapSphere();
-	// 2. OnTriggerEnter(); <- 이걸로 선택
-	// 이유 ? -> 일단 없는데
-	// 1번으로 선회 -> 이유 : 안된다
-
-	[Tooltip("공격 범위에 들어온 적 캐릭터")]
-	public SharedGameObject Target;
-
-	[SerializeField] Collider2D[] enemies;
-	[SerializeField] float attackRange;
-	[SerializeField] Vector2 range;
+    [SerializeField] Collider2D[] characters;
+    [SerializeField] float attackRange;
+    [SerializeField] Vector2 range;
+    private Vector2 pos;
+    private SpriteRenderer renderer;
 
     public override void OnAwake()
     {
         range = new Vector2(1f, 7.5f);
-        enemies = new Collider2D[1];
+        characters = new Collider2D[10];
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public override void OnEnd()
+    {
+        if (string.IsNullOrEmpty(TargetTag.Value))
+        {
+            TargetTag.Value = gameObject.CompareTag("Enemy") ? "Player" : "Enemy";
+        }
     }
 
     public override TaskStatus OnUpdate()
-	{
-		CheckEnemyInAttackArea();
+    {
+        CheckEnemyInAttackArea();
 
-		return Target.Value != null ? TaskStatus.Success : TaskStatus.Failure;
-	}
+        return Target.Value != null ? TaskStatus.Success : TaskStatus.Failure;
+    }
 
-	public void CheckEnemyInAttackArea()
-	{
-		// 이미 공격타겟이 있으면 확인하는 로직 실행 x
-		if (Target.Value != null) return;
+    public void CheckEnemyInAttackArea()
+    {
+        // 이미 공격타겟이 있으면 확인하는 로직 실행 x
+        if (Target.Value != null) return;
 
         Debug.Log("연산은 하냐");
 
+        for(int i = 0; i < characters.Length; i++) characters[i] = null;
+
+        // 공격 범위
         range = new Vector2(attackRange, 7.5f);
-		enemies[0] = null;
-		Physics2D.OverlapBoxNonAlloc(Vector2.right * transform.position.x + Vector2.right,  range, 0f, enemies, LayerMask.GetMask("Character"));
 
-		if (enemies[0] != null)
-		{
-			Target.Value = enemies[0].gameObject;
-		}
-	}
+        // 탐색할 위치 = 현재 캐릭터의 위치(x) + 공격 범위의 반절
+        pos = new Vector2(transform.position.x + attackRange * (renderer.flipX ? -1f : 1f), 0f);
 
-    public override void OnCollisionEnter2D(Collision2D other)
-    {
-		if(Target.Value == null && other.collider.gameObject.CompareTag("Enemy")) Target.Value = other.gameObject;
-    }
+        // 공격 범위에 들어온 캐릭터의 수
+        int count = Physics2D.OverlapBoxNonAlloc(
+                pos,
+                range, 
+                0f, 
+                characters, LayerMask.GetMask("Character")
+            );
 
-    public override void OnTriggerExit2D(Collider2D other)
-    {
-        if (Target.Value != null 
-			&& Target.Value.Equals(other.gameObject)
-            && other.gameObject.CompareTag("Enemy")) Target.Value = null;
+        if (count > 0)
+        {
+            for (int i = 0; i < characters.Length; i++)
+            {
+                // 순차적으로 들어와서 없을 경우 더이상 탐색이 불필요
+                if (characters[i] == null) break;
+                // 공격 범위에 들어온 캐릭터의 태그가 나와 적대 관계의 태그라면
+                if (characters[i].gameObject.CompareTag(TargetTag.Value))
+                {
+                    // 해당 대상을 타겟
+                    Target.Value = characters[i].gameObject;
+                    break;
+                }
+            }
+        }
     }
 
     public override void OnDrawGizmos()
     {
 #if UNITY_EDITOR
-		Gizmos.color = Color.red;
-		Gizmos.DrawWireCube (Vector2.right * Owner.transform.position.x + Vector2.right, (Vector2.right + Vector2.up * 7.5f));
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector2( Owner.transform.position.x + (Owner.GetComponent<SpriteRenderer>().flipX ? -1f : 1f), 0f), (Vector2.right * (Owner.CompareTag("Player") ? 1 : -1) + Vector2.up * 7.5f));
 #endif
     }
 
--- Assets/Scripts/MoveAction.cs
+++ Assets/Scripts/MoveAction.cs
@@ -10,19 +10,22 @@ public class MoveAction : Action
 	public SharedBool IsMoveAnimStart;
     public SharedBool IsAttackAnimStart;
 
-    [SerializeField] protected float moveSpeed;
-	protected Animator animator;
-	
+    [SerializeField] float moveSpeed;
+	private Animator animator;
+	private SpriteRenderer renderer;
 
     public override void OnAwake()
     {
 		animator = GetComponent<Animator>();
+		renderer = GetComponent<SpriteRenderer>();
     }
 
     public override TaskStatus OnUpdate()
 	{
 		if (IsMoveAnimStart.Value.Equals(false)) MoveAnimation();
+
 		Move();
+
 		return TaskStatus.Success;
 	}
 
@@ -30,7 +33,7 @@ public class MoveAction : Action
 	{
 		if (IsAttackAnimStart.Value.Equals(true)) return;
 
-		transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+		transform.Translate(Vector2.right * moveSpeed * Time.deltaTime * (renderer.flipX ? -1 : 1 ));
 		if(Camera.main.WorldToViewportPoint(transform.position).x > 1f)
 		{
 			Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
@@ -44,7 +47,7 @@ public class MoveAction : Action
 		// 움직이는 애니메이션 진행
 		// 이게 계속하면 안되서 한번만 진행해야 함
 		// 각 테스크 별로 작동을 해야하니 -> 공용 변수를 사용하여 조절
-		animator.CrossFade(MoveAnimationName.Value, 0.01f);
+		animator.CrossFade(MoveAnimationName.Value, 0f);
 		IsMoveAnimStart.Value = true;
         IsAttackAnimStart.Value = false;
     }
