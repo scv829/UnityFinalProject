ec08702fef1897288c941aaacac464a9e580fab8
scv829 <scv829@naver.com>
2025-04-07 06:05:55 +0900

    Refactor: 캐릭터 데이터 형식 수정
    캐릭터 데이터 형식 수정
    
    Related to: #9

--- Assets/Scripts/Character/Base/CharacterSO.cs
+++ Assets/Scripts/Character/Base/CharacterSO.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using static CharacterEnum;
 
+
 [CreateAssetMenu(menuName = "Character/Data")]
 public class CharacterSO : ScriptableObject
 {
+    /* 기존 코드
     [SerializeField] int character_id;
     [SerializeField] string character_name;
     [SerializeField] int character_star;
@@ -26,5 +28,21 @@ public class CharacterSO : ScriptableObject
     public float Damage => damage;
     public List<Skill> CharacterSkill => character_skill;
     #endregion
+    */
 
+    // 캐릭터 이름
+    public string Name;
+    // 캐릭터 등급
+    [Range(1, 5)]
+    public int Rarity;
+    // 캐릭터 위치
+    public CharacterEnum.PositionType Position;
+    // 캐릭터 체력
+    public float Hp;
+    // 캐릭터 공격력
+    public float ATK;
+    // 캐릭터 방어력
+    public float Def;
+    // 캐릭터 이동 속도
+    public float MoveSpeed;
 }
