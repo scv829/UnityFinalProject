a77921df7b8ad0b39eeb7052ece0b10d66d2fd15
scv829 <scv829@naver.com>
2025-04-07 06:11:35 +0900

    Refactor: 캐릭터 관리자 로직 수정
    캐릭터 관리자 로직 수정
    
    Related to: #9

--- Assets/Scripts/Character/Base/CharacterHandler.cs
+++ Assets/Scripts/Character/Base/CharacterHandler.cs
@@ -8,40 +8,88 @@ using UnityEngine;
 public class CharacterHandler : MonoBehaviour, IHit
 {
     [Header("프로퍼티")]
-    [SerializeField] CharacterSO characterData;
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer render;
     [SerializeField] BehaviorTree bt;
 
+    [Header("스팩")]
+    [SerializeField] CharacterSO characterData;
+    [SerializeField] float HP;
+    [SerializeField] float ATK;
+    [SerializeField] float DEF;
+    [SerializeField] float moveSpeed;
+
     [Header("전투")]
     [SerializeField] float currentHp;
     [SerializeField] bool isInitComplete;
+    [SerializeField] BattleManager battleManager;
 
     [Header("스킬")]
-    [SerializeField] int index;
-    [SerializeField] GameObject target;
-    [SerializeField] List<float> coolTimeList;
-    [SerializeField] List<float> currentCoolTimeList;
+    [SerializeField] SkillHandler skillHandler;
 
+    [Header("장비")]
+    [SerializeField] Armor[] armors;
+    [SerializeField] Weapon weapon;
+
+    public BattleManager BattleManager { 
+        set 
+        { 
+            battleManager = value; 
+        } 
+    }
+
+    public CharacterSO Data
+    {
+        set { if(value != null) characterData = value; }
+    }
+
+
+    /// <summary>
+    /// 캐릭터 생싱 초기화 함수
+    /// </summary>
+    /// <param name="tag">아군/적군 식별</param>
+    public void Init(string tag)
+    {
+        gameObject.tag = tag;
+        render.flipX = gameObject.CompareTag("Enemy");
+
+        foreach(Armor armor in armors)
+        {
+            // 착용 방어구에 따른 스탯 추가량
+            switch (armor.ArmorType)
+            {
+                case ArmorType.Head:
+                case ArmorType.Shoes:
+                    HP = characterData.Hp += armor.GetAddAmount();
+                    break;
+                case ArmorType.Top:
+                case ArmorType.Bottom:
+                    DEF = characterData.Def += armor.GetAddAmount();
+                    break;
+                case ArmorType.Gloves:
+                    ATK = characterData.ATK += armor.GetAddAmount();
+                    break;
+            }
+        }
+
+        moveSpeed = characterData.MoveSpeed;
+
+        currentHp = characterData.Hp;
+
+        skillHandler.InitSkillData();
+
+        isInitComplete = true;
+    }
+
+    public int SpawnPos => (int)characterData.Position;
 
     private void Awake()
     {
-        coolTimeList = new List<float>();
-        currentCoolTimeList = new List<float>();
         animator = GetComponent<Animator>();
         render = GetComponent<SpriteRenderer>();
         bt = GetComponent<BehaviorTree>();
+        skillHandler = GetComponent<SkillHandler>();
         isInitComplete = false;
-    }
-
-    private void Start()
-    {
-        foreach (Skill skill in characterData.CharacterSkill)
-        {
-            coolTimeList.Add(skill.SkillCoolTime);
-            currentCoolTimeList.Add(skill.SkillCoolTime);
-        }
-        currentHp = characterData.HP;
     }
 
     public bool TakeDamage(GameObject requester, BattleEnum.DamageType type, float damage)
@@ -56,85 +104,41 @@ public class CharacterHandler : MonoBehaviour, IHit
     /// </summary>
     public bool IsDied => currentHp <= 0;
 
-    public float Damage => characterData.Damage;
+    public float Damage => (ATK + weapon.GetAddAmount());
 
     private void Update()
     {
         if (IsDied)
         {
+            // 사망 로직
+            battleManager.OnDied(gameObject);
+
             gameObject.layer = LayerMask.NameToLayer("Die");
             animator.CrossFade("Die", 0f);
-
-            NotifyDeath(); // 스테이지 매니저에게 사망 알림
-        }
-        CoolTime();
-    }
-
-    private void CoolTime()
-    {
-        for (int i = 0; i < currentCoolTimeList.Count; i++)
-        {
-            if (currentCoolTimeList[i] <= 0f) continue;
-            currentCoolTimeList[i] -= Time.deltaTime;
         }
     }
-
-    public bool CanUseSkill(out int index)
-    {
-        index = -1;
-        for (int i = 0; i < currentCoolTimeList.Count; i++)
-        {
-            // 같이 호출이 된다
-            if (currentCoolTimeList[i] <= 0f)
-            {
-                Debug.Log($"{i}번 스킬 사용 가능!");
-                index = i;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void SkillAction(in int index, GameObject target)
-    {
-        if (index == -1 || target == null) return;
-
-        this.index = index;
-        this.target = target;
-
-        // 애니메이션 시작
-        animator.CrossFade(characterData.CharacterSkill[index].AnimationName, 0f);
-        currentCoolTimeList[index] = coolTimeList[index];
-    }
-
-    public void Action() => characterData.CharacterSkill[index].Action(this, target);
 
     /// <summary>
-    /// 캐릭터 생싱 초기화 함수
+    /// 사용 가능한 스킬 여부 확인 호출 함수
     /// </summary>
-    /// <param name="tag">아군/적군 식별</param>
-    public void Init(string tag)
+    /// <param name="index">사용가능 한 스킬 인덱스</param>
+    /// <returns>사용 가능 여부</returns>
+    public bool CanUseSkill(out int index)
     {
-        gameObject.tag = tag;
-        render.flipX = gameObject.CompareTag("Enemy");
-
-        // 여기서 데이터를 세팅해야 하나?
-        // 
-        
-
-        isInitComplete = true;
+        index = skillHandler.CanUseSkill();
+        return (index.Equals(-1)) ? false : true;
     }
 
-    public void SetData()
-    {
-        // 정보 세팅
-    }
-
-    private void NotifyDeath()
-    {
-        // StageManager의 캐릭터 사망 처리 호출
-        StageManager.Instance.OnCharacterDeath(this, gameObject.tag);
-    }
+    /// <summary>
+    /// 스킬 사용 호출
+    /// </summary>
+    /// <param name="index">사용할 스킬의 인덱스</param>
+    /// <param name="target">공격을 맞을 타겟</param>
+    public void UseSkill(in int index, GameObject target) => skillHandler.UseSkill(index, target);
 
     public bool IsInitComplete => isInitComplete;
+
+    public float AttackRange => weapon.AttackRange;
+    public float AttackSpeed => weapon.AttackSpeed;
+    public int AttackAnimHash => weapon.AttackAnimHash;
 }
